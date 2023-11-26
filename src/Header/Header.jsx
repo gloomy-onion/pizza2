924e9d5464fdb7a9ee99d483c113bc6cc6160eb2
Ira <nikulochkina.irina@yandex.ru>
2023-11-26 15:11:24 +0300

    изучили хук useLocation

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import pizzaLogo from './../img/pizzaLogo.svg';
 import styles from './Header.module.scss';
 import cartHeader from '../img/cartHeader.svg';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import Search from '../Search/Search';
 import {useSelector} from 'react-redux';
 import {selectCart} from '../redux/Slices/cartSlice';
 
 const Header = () => {
   const {totalPrice, totalCount} = useSelector(selectCart);
+  const location = useLocation();
   return (
     <div className={styles.header}>
       <div className={styles.container}>
@@ -23,13 +24,13 @@ const Header = () => {
         </Link>
         <Search/>
         <div className={styles.headerCart}>
-          <Link to={'/cart'} className={styles.button}>
+          {location.pathname !== '/cart' && <Link to={'/cart'} className={styles.button}>
             <span>{totalPrice} ₽</span>
             <div className={styles.button__delimiter}>
             </div>
             <img src={cartHeader} alt={'cart'}/>
             <span>{totalCount}</span>
-          </Link>
+          </Link>}
         </div>
       </div>
     </div>
